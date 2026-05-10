f363a7945ae449d9808f3de24df4ae9c47b91e33
sonaa1107 <sonaagrawal1107@gmail.com>
2026-05-11 00:23:03 +0530

    Time: 0 ms (100%), Space: 42.7 MB (99.55%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left=0;
+        int mid=0;
+        int right=nums.length-1;
+        while(mid<=right){
+            if(nums[mid]==2){
+                swap(nums,mid,right);
+                right--;
+            }
+            else if(nums[mid]==0){
+                swap(nums,mid,left);
+                left++;
+                mid++;
+            }else{
+                mid++;
+            }
+        }
+    }
+    void swap(int[]nums,int i,int j){
+        int t=nums[i];
+        nums[i]=nums[j];
+        nums[j]=t;
+    }
+}
