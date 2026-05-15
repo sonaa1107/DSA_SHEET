c8d2de8cc881e4cb18e8c1a3f98fba54ff27fb0c
sonaa1107 <sonaagrawal1107@gmail.com>
2026-05-15 11:55:05 +0530

    Time: 216 ms (13.69%), Space: 46.7 MB (14.03%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -2,33 +2,35 @@ class Solution {
     public boolean exist(char[][] board, String word) {
         int m=board.length;
         int n=board[0].length;
-        boolean[][]vis=new boolean[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word.charAt(0)){
-                    if(helper(board,word,i,j,1,vis))
+                    if(helper(board,word,i,j,1))
                         return true;
                 }
             }
         }
         return false;
     }
-    boolean helper(char[][]b,String word,int i,int j,int c,boolean[][]vis){
+    boolean helper(char[][]b,String word,int i,int j,int c){
         if(c==word.length()){
             return true;
         }
-        vis[i][j]=true;
+        char t=b[i][j];
+        b[i][j]='#'; //mark visited
         int[][]dir={{-1,0},{0,1},{1,0},{0,-1}};
         for(int[]d:dir){
             int ni=i+d[0];
             int nj=j+d[1];
 
-            if(ni>=0 && ni<b.length && nj>=0 && nj<b[0].length && !vis[ni][nj] && b[ni][nj]==word.charAt(c)){
-                if(helper(b,word,ni,nj,c+1,vis))
+            if(ni>=0 && ni<b.length && nj>=0 && nj<b[0].length && b[ni][nj]==word.charAt(c)){
+                if(helper(b,word,ni,nj,c+1)){
+                    b[i][j]=t; // restore before returning
                     return true;
+                }
             }
         }
-        vis[i][j]=false;
+        b[i][j]=t; //backtrack
         return false;
     }
 }
