7d355a7965644213b8c210f0f72b85b6f3a80dfc
sonaa1107 <sonaagrawal1107@gmail.com>
2026-05-18 10:54:05 +0530

    Time: 1 ms (99.59%), Space: 46.4 MB (99.77%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int jump(int[] nums) {
+        int l=0;
+        int r=0;
+        int jump=0;
+        int n=nums.length;
+        while(r<n-1){
+            int farthest=0;
+            for(int i=l;i<=r;i++){
+                farthest=Math.max(farthest,i+nums[i]);
+            }
+            l=r+1;
+            r=farthest;
+            jump++;
+        }
+        return jump;
+    }
+}
